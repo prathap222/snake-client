0230793afff99314f37bce39f55081ac83fa3cd9
Prathap <prathap.sims@gmail.com>
2022-07-07 06:04:29 +0000

    added funny messages

--- input.js
+++ input.js
@@ -13,14 +13,20 @@ const setupInput = (conn) => {
 const handleUserInput = (input) => {
   if (input === '\u0003') {
     process.exit();
-  } else if( input === 'w') {
+  } else if(input === 'w') {
     connection.write('Move: up');
-  } else if( input === 'a') {
+  } else if(input === 'a') {
     connection.write('Move: left');
-  } else if( input === 's') {
+  } else if(input === 's') {
     connection.write('Move: down');
-  } else if( input === 'd') {
+  } else if(input === 'd') {
     connection.write('Move: right');
+  } else if (input === 'f') {
+    connection.write('Say: You are funny!!!');
+  } else if (input === 'r') {
+    connection.write('Say: Noooo... turn turn!!!');
+  } else if (input === 'q') {
+    connection.write('Say: hahahhaha!!!');
   }
 };
 
