9e3a3f8e2664688a5afabe349677e0ea0c30214d
Prathap <prathap.sims@gmail.com>
2022-07-07 05:57:26 +0000

    added key movements

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,9 +10,17 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (input) {
+const handleUserInput = (input) => {
   if (input === '\u0003') {
     process.exit();
+  } else if( input === 'w') {
+    connection.write('Move: up');
+  } else if( input === 'a') {
+    connection.write('Move: left');
+  } else if( input === 's') {
+    connection.write('Move: down');
+  } else if( input === 'd') {
+    connection.write('Move: right');
   }
 };
 
