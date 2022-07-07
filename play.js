4a23b0589abba56580ad48b6246685452eb8cd23
Prathap <prathap.sims@gmail.com>
2022-07-07 04:58:45 +0000

    updated with move commands

--- play.js
+++ play.js
@@ -1,22 +1,21 @@
-// const net = require("net");
-
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "10.0.2.15", // IP address here,
-//     port: 50541 // PORT number here,
-//   });
-//   conn.on('data', (data)=>{
-//     console.log(data);
-//   })
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   return conn;
-// };
 
 const connect = require('./client');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', userInput)
+  return stdin;
+};
+
+const userInput = (input) => {
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+
 console.log("Connecting ...");
+setupInput();
 connect();
