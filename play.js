cab42caa509d238b14214872363710f318a0845a
Prathap <prathap.sims@gmail.com>
2022-07-07 05:30:17 +0000

    refactored to use input.js

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 
 const connect = require('./client');
+const  setupInput = require('./input');
 
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', userInput)
-  return stdin;
-};
-
-const userInput = (input) => {
-  if (input === '\u0003') {
-    process.exit();
-  }
-};
 
 console.log("Connecting ...");
 setupInput();
