2e6fe68d433c9b533b95b546f295c5bcbaa4ead0
Prathap <prathap.sims@gmail.com>
2022-07-07 04:58:28 +0000

    updated with move commands

--- client.js
+++ client.js
@@ -1,21 +1,39 @@
 const net = require('net');
 
-const connect = function() {
+
+    const connect = () => {
+      const name = 'Name: SP';
+      const moveUp = 'Move: up';
+      const moveRight = 'Move: right';
+      const moveDown = 'Move: down';
+      const moveLeft = 'Move: left';
+
   const conn = net.createConnection({
     host: "10.0.2.15", 
     port: 50541
   });
 
-  conn.setEncoding('utf8'); //interpret data as txt
-  conn.write(`spp`);
+  conn.setEncoding('utf8'); 
+  
+  conn.write(`${name}`);
+
+
+  setInterval(() => { conn.write(`${moveUp}`)}, 3000);
+  setInterval(() => { conn.write(`${moveRight}`)}, 1000);
+  setInterval(() => { conn.write(`${moveDown}`)}, 1000);
+  setInterval(() => { conn.write(`${moveLeft}`)}, 1000);
 
   conn.on('data', (data) => {
-    console.log("Successfully connected to game server" ,data);
-  })
+    console.log("Received: " + data);
+  });
+
+  conn.on('connect', (data) => {
+    console.log("You are connected :)");
+  });
 
 
 
   return conn;
-};
+}
 
 module.exports = connect;
